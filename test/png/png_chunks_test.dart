1d3740fa4b734526c9ae20e39e92cc74aaf92175
itc-s22020 <s22020@std.it-college.ac.jp>
2024-06-19 13:56:04 +0900

    テストコードを修正

--- test/png/png_chunks_test.dart
+++ test/png/png_chunks_test.dart
@@ -1,21 +1,31 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:polyglot/png/png_chunks_extract.dart';
 
 void main() {
-  test('display PNG chunks', () {
-    var dir = Directory.current.path;
+  late String dir;
+  late final File file;
+  late final String res;
+  late final Uint8List data;
+
+  setUp((){
+    dir = Directory.current.path;
     if (dir.endsWith('/test')) {
       dir = dir.replaceAll('/test', '');
     }
+    res = 'test/png/res';
+    file = File('$dir/$res/default.png');
+    data = file.readAsBytesSync();
+  });
 
-    File file = File('$dir/test/png/res/cleaned.png');
-    final data = file.readAsBytesSync();
+  test('display PNG chunks', () {
     final chunks = extractChunks(data);
-
-    for (var chunk in chunks) {
-        print('Chunk name: ${chunk['name']}');
-        print('Chunk data length: ${chunk['data'].length}');
+    for (final Map<String, dynamic> chunk in chunks) {
+        if (kDebugMode) {
+          print('Chunk name: ${chunk['name']}');
+          print('Chunk data length: ${chunk['data'].length}');
+        }
     }
   });
 }
--- test/png/png_clean_test.dart
+++ test/png/png_clean_test.dart
@@ -6,27 +6,34 @@ import 'package:polyglot/png/png_chunks_clean.dart';
 import 'package:polyglot/png/png_chunks_extract.dart';
 
 void main() {
-  test('clean PNG chunks', () {
-    var dir = Directory.current.path;
+  late String dir;
+  late final File file;
+  late final String res;
+  late final Uint8List data;
+
+  setUp(() {
+    dir = Directory.current.path;
     if (dir.endsWith('/test')) {
       dir = dir.replaceAll('/test', '');
     }
+    res = 'test/png/res';
+    file = File('$dir/$res/default.png');
+    data = file.readAsBytesSync();
+  });
 
-    File file = File('$dir/test/png/res/default.png');
-    final data = file.readAsBytesSync();
-    Uint8List cleanedData = cleanChunks(data);
-
-    File outputFile = File('$dir/test/png/res/cleaned.png');
+  test('clean PNG chunks', () {
+    final Uint8List cleanedData = cleanChunks(data);
+    final File outputFile = File('$dir/$res/cleaned.png');
     outputFile.writeAsBytesSync(cleanedData);
 
-    List<Map<String, dynamic>> chunks = extractChunks(cleanedData);
-    for (var chunk in chunks) {
+    final List<Map<String, dynamic>> chunks = extractChunks(cleanedData);
+    for (final Map<String, dynamic> chunk in chunks) {
       expect(chunk['name'], anyOf(['IHDR', 'IDAT', 'IEND']));
     }
 
     expect(chunks.first['name'], 'IHDR');
     expect(chunks.last['name'], 'IEND');
-    var idatChunks = chunks.where((chunk) => chunk['name'] == 'IDAT').toList();
+    final List<Map<String, dynamic>> idatChunks = chunks.where((chunk) => chunk['name'] == 'IDAT').toList();
     expect(idatChunks.length, 1);
   });
 }
